92b14aed63442f2cbff5b4adab785b8e054e0815
NadezhdaDudnik <nadin869@mail.ru>
2024-07-24 00:25:32 +0300

    Редактирование gitignore файла, добавление теста и файла requirements.txt

--- qa_guru_python_15_2/lesson_2_git.py
+++ qa_guru_python_15_2/lesson_2_git.py
@@ -1,1 +1,6 @@
-pass
+from selene import browser, be, have
+
+
+browser.open('https://google.com')
+browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
