587e828e1560f8299e572d3eb4cd686792cd30cc
NadezhdaDudnik <nadin869@mail.ru>
2024-08-15 23:14:39 +0300

    исправление задания 5

--- tests/test_hw_demoqa.py
+++ tests/test_hw_demoqa.py
@@ -1,24 +1,42 @@
 import os
-from selene import browser
+from selene import (
+    browser,
+    command,
+    have,
+)
 
 
-def test_homework_demoqa():
+def test_registration_form():
     browser.open('/automation-practice-form')
+    browser.element('#fixedban').perform(command.js.remove)
+    browser.element('footer').perform(command.js.remove)
     browser.element('#firstName').type('Nadezhda')
     browser.element('#lastName').type('Dudnik')
-    browser.element('#userEmail').type('nadin869@mail.ru')
+    browser.element('#userEmail').type('nadintest_test@mail.ru')
     browser.element('[for="gender-radio-2"]').click()
-    browser.element('#userNumber').type('89081145057')
+    browser.element('#userNumber').type('8995114236')
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__year-select option[value="1986"]').click()
     browser.element('.react-datepicker__month-select option[value="7"]').click()
     browser.element('.react-datepicker__day.react-datepicker__day--002').click()
-    browser.element('#subjectsInput').type('Computer Science').press_enter()
+    browser.element('#subjectsInput').type('Computer Science').press_tab()
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('it.jpg'))
     browser.element('#currentAddress').type('Moscow')
-    browser.element('#state').click().element('#react-select-3-option-3').click()
-    browser.element('#city').click().element('#react-select-4-option-1').click()
+    browser.element("#react-select-3-input").type('Haryana').press_enter()
+    browser.element('#react-select-4-input').type('Karnal').press_enter()
     browser.element('#submit').click()
-
-
+    browser.element('.modal-content').element('table').all('tr').all('td').even.should(
+        have.exact_texts(
+            'Nadezhda Dudnik',
+            'nadintest_test@mail.ru',
+            'Female',
+            '8995114236',
+            '02 August,1986',
+            'Computer Science',
+            'Sports',
+            'it.jpg',
+            'Moscow',
+            'Haryana Karnal',
+        )
+    )
