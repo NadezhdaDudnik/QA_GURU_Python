ef794969c72f74df7b9687a81baf67e2d1440f6b
NadezhdaDudnik <nadin869@mail.ru>
2024-08-19 23:47:41 +0300

    исправление задания 6

--- test_homework_6_python.py
+++ test_homework_6_python.py
@@ -20,10 +20,9 @@ def test_dark_theme_by_time_and_user_choice():
     """
     current_time = time(hour=10)
     dark_theme_enabled_by_user = True
-    if dark_theme_enabled_by_user is not None:
-        is_dark_theme = dark_theme_enabled_by_user
-    else:
-        is_dark_theme = not (6 <= current_time.hour < 22)
+    is_dark_theme = current_time.hour not in range(
+        6, 22
+        ) if dark_theme_enabled_by_user is None else dark_theme_enabled_by_user
     assert is_dark_theme is True
 
 
