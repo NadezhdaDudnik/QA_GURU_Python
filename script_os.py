74bc09f3996bb0c173b2eb7565e0b34897c981dc
NadezhdaDudnik <nadin869@mail.ru>
2024-08-26 23:07:46 +0300

    исправление задания 7

--- script_os.py
+++ script_os.py
@@ -4,4 +4,4 @@ CURRENT_FILE = os.path.abspath(__file__)
 FILES2_DIR = os.path.join(os.path.dirname(CURRENT_FILE), 'files2')
 ARCHIVE_FILE = os.path.join(FILES2_DIR, 'archive.zip')
 FILES_DIR = os.path.join(os.path.dirname(CURRENT_FILE), 'files')
-files = ['test_task.xlsx', 'test_task.csv', 'test_task.pdf']
+
--- test_archive_file.py
+++ test_archive_file.py
@@ -4,11 +4,9 @@ import csv
 from pypdf import PdfReader
 from openpyxl import load_workbook
 
-from script_os import (
-    ARCHIVE_FILE,
-    FILES_DIR,
-)
+from script_os import ARCHIVE_FILE
 
+files = ['test_task.xlsx', 'test_task.csv', 'test_task.pdf']
 
 def normalize_text(text):
     return ' '.join(text.split())
@@ -16,7 +14,7 @@ def normalize_text(text):
 
 def test_archive_csv():
     with ZipFile(ARCHIVE_FILE, 'r') as zip_file:
-        with zip_file.open(FILES_DIR) as csv_file:
+        with zip_file.open('test_task.csv') as csv_file:
             csv_content = csv_file.read().decode('utf-8-sig')
             csvreader = list(csv.reader(csv_content.splitlines()))
             row_1 = csvreader[1]
@@ -26,7 +24,7 @@ def test_archive_csv():
 
 def test_xlsx():
     with ZipFile(ARCHIVE_FILE, 'r') as zip_file:
-        with zip_file.open(FILES_DIR) as xlsx_file:
+        with zip_file.open('test_task.xlsx') as xlsx_file:
             workbook = load_workbook(xlsx_file)
             sheet = workbook.active
             cell_text = sheet.cell(row=3, column=1).value
@@ -36,7 +34,7 @@ def test_xlsx():
 
 def test_pdf():
     with ZipFile(ARCHIVE_FILE, 'r') as zip_file:
-        with zip_file.open(FILES_DIR) as pdf_file:
+        with zip_file.open('test_task.pdf') as pdf_file:
             reader = PdfReader(pdf_file)
             page_text = reader.pages[1].extract_text()
 
